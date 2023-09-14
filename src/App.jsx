290a6a10aeb87ece3b3da227e2943eab7a20066c
loinq2704 <nguyenquangloi2704@gmail.com>
2023-09-14 08:14:52 +0700

    Update library

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,11 @@ import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import { LoginPage } from './components/authent/login/LoginPage'
 function App() {
-
-
   return (
     <>
       <Routes>
         <Route path='/login' Component={LoginPage}/>
+        <Route path="student/*" element={<StudentPage />} />
       </Routes>
     </>
   )
