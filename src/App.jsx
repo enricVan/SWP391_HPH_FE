71fa9abd6139200337e85a5dc8c5cd292d097385
loinq2704 <nguyenquangloi2704@gmail.com>
2023-09-14 08:25:55 +0700

    Resolve conflict

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import { LoginPage } from './components/authent/login/LoginPage'
+import Dashboard from './components/dashboard/student/StudenPage' 
 function App() {
   return (
     <>
       <Routes>
         <Route path='/login' Component={LoginPage}/>
-        <Route path="student/*" element={<StudentPage />} />
+        <Route path="student/*" element={<Dashboard />} />
       </Routes>
     </>
   )
