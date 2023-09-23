24ce3d151df10aa58b734b4dcd62459fc2e09261
ThaoTTP123 <dreemmurrchara@gmail.com>
2023-09-23 11:47:46 +0700

    asf

--- src/pages/student/news/NewsDetail.jsx
+++ src/pages/student/news/NewsDetail.jsx
@@ -1,13 +1,14 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Box, Paper } from "@mui/material";
 import { useParams } from "react-router-dom";
 import newsService from "../../../service/newsService";
 import DocViewer, { DocViewerRenderers } from "@cyntler/react-doc-viewer";
-import * as mod from "./pdf/*";
+import dynamicModule from "./pdf/FERproject.pdf";
+// import * as mod from "./pdf/*";
 export default function NewsDetail() {
   const { id } = useParams();
   const newsDetail = newsService.getNewsById();
-  const docs = [{ uri: mod }];
+  const docs = [{ uri: dynamicModule }];
   return (
     <Box padding={1}>
       <h1 style={{ marginLeft: "8px" }}>Room Assignment Requests</h1>
