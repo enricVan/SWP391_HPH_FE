f49e8ebb523dc1c8de22be5d3452e332e4b720dd
ThaoTTP123 <dreemmurrchara@gmail.com>
2023-09-24 23:47:51 +0700

    news-feature

--- src/pages/student/news/NewsDetail.jsx
+++ src/pages/student/news/NewsDetail.jsx
@@ -3,12 +3,16 @@ import { Box, Paper } from "@mui/material";
 import { useParams } from "react-router-dom";
 import newsService from "../../../service/newsService";
 import DocViewer, { DocViewerRenderers } from "@cyntler/react-doc-viewer";
-import dynamicModule from "./pdf/FERproject.pdf";
 // import * as mod from "./pdf/*";
 export default function NewsDetail() {
-  const { id } = useParams();
-  const newsDetail = newsService.getNewsById();
-  const docs = [{ uri: dynamicModule }];
+  const [file, setFile] = useState(null);
+  let { id } = useParams();
+  const newsDetail = newsService.getNewsById(Number(id));
+  console.log(newsDetail, id);
+  import(`./pdf/${newsDetail.file}`).then((module) => {
+    setFile(module.default);
+  });
+  const docs = [{ uri: file, fileName: "Announcement" }];
   return (
     <Box padding={1}>
       <h1 style={{ marginLeft: "8px" }}>Room Assignment Requests</h1>
