731732f1c7be9a3921b2d2f0ddbb82aee04aea74
ThaoTTP123 <dreemmurrchara@gmail.com>
2023-10-17 17:25:42 +0700

    temp

--- src/pages/student/bedbooking/BedBooking.jsx
+++ src/pages/student/bedbooking/BedBooking.jsx
@@ -25,7 +25,6 @@ import {
 } from "@mui/material";
 import { Link } from "react-router-dom";
 import { privateAxios } from "../../../service/axios";
-import { useSelector } from "react-redux";
 const BootstrapDialog = styled(Dialog)(({ theme }) => ({
   "& .MuiDialogContent-root": {
     padding: theme.spacing(2),
@@ -34,9 +33,9 @@ const BootstrapDialog = styled(Dialog)(({ theme }) => ({
     padding: theme.spacing(1),
   },
 }));
+const user = JSON.parse(localStorage.getItem("user"));
 export default function BedBooking() {
   const [open, setOpen] = React.useState(false);
-  const { user } = useSelector((state) => state.user);
   const handleClickOpen = () => {
     const requestData = {
       bed: {
@@ -45,9 +44,11 @@ export default function BedBooking() {
       student: {
         studentId: user.id,
       },
-      semester: semester,
+      semester: {
+        semesterId: semester.semesterId,
+      },
     };
-    privateAxios.post("bedRequest", requestData);
+    privateAxios.post("bed-request", requestData);
     setOpen(true);
   };
   const handleClose = () => {
@@ -67,9 +68,9 @@ export default function BedBooking() {
     const res2 = await privateAxios.get("room-type");
     const roomData = await res2;
     settypes(roomData.data);
-    const res3 = await privateAxios.get("semester/nextSemester");
+    const res3 = await privateAxios.get("semester/next-semester");
     const semesterData = await res3;
-    setSemester(semesterData.data.semesterName);
+    setSemester(semesterData.data);
   };
   useEffect(() => {
     fetchData();
@@ -82,7 +83,8 @@ export default function BedBooking() {
     }
   };
   const handleChange = (e) => {
-    if (e.target.value) setRoomType(e.target.value);
+    if (e.target.value === "All") setRoomType("");
+    else setRoomType(e.target.value);
   };
   return (
     <Box>
@@ -93,11 +95,11 @@ export default function BedBooking() {
           <Select
             id="select"
             label="Room Type"
-            value={roomType}
+            value={roomType === "" ? "All" : roomType}
             labelId="roomType-label"
             onChange={handleChange}
           >
-            <MenuItem value="">All</MenuItem>
+            <MenuItem value="All">All</MenuItem>
             {types.map((type) => {
               return (
                 <MenuItem key={type.roomTypeId} value={type.roomTypeId}>
@@ -133,7 +135,7 @@ export default function BedBooking() {
           variant="h5"
           color={"orangered"}
         >
-          Semester: {semester}
+          Semester: {semester.semesterName}
         </Typography>
       </Box>
       <Box padding={1}>
@@ -144,7 +146,7 @@ export default function BedBooking() {
                 <TableCell>ID</TableCell>
                 <TableCell align="center">Bed</TableCell>
                 <TableCell align="center">Room</TableCell>
-                <TableCell align="center">Dorm</TableCell>
+                <TableCell align="center">Building</TableCell>
                 <TableCell align="center">Floor</TableCell>
                 <TableCell align="center">Price&nbsp;(VND)</TableCell>
                 <TableCell align="center"></TableCell>
@@ -169,9 +171,11 @@ export default function BedBooking() {
                     </TableCell>
                     <TableCell align="center">{bed.bedName}</TableCell>
                     <TableCell align="center">{bed.room.roomName}</TableCell>
-                    <TableCell align="center">{bed.room.belongDom}</TableCell>
+                    <TableCell align="center">
+                      {bed.room.building.buildingName}
+                    </TableCell>
                     <TableCell align="center">{bed.room.floor}</TableCell>
-                    <TableCell align="center">{bed.price}</TableCell>
+                    <TableCell align="center">{bed.room.roomPrice}</TableCell>
                     <TableCell align="center">
                       <Radio
                         checked={selectedValue === bed.bedId}
--- src/pages/student/bookedhistory/BookedHistory.jsx
+++ src/pages/student/bookedhistory/BookedHistory.jsx
@@ -8,27 +8,25 @@ import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import Box from "@mui/material/Box";
 import { privateAxios } from "../../../service/axios";
-import { useSelector } from "react-redux";
+import {
+  FormControl,
+  IconButton,
+  InputLabel,
+  MenuItem,
+  Select,
+} from "@mui/material";
+import { Remove, RemoveRedEye } from "@mui/icons-material";
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-  createData("Eclair", 262, 16.0, 24, 6.0),
-  createData("Cupcake", 305, 3.7, 67, 4.3),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
-];
+const statusList = ["All", "pending", "approved", "rejected"];
+const user = JSON.parse(localStorage.getItem("user"));
 export default function BookedHistory() {
-  const { user } = useSelector((state) => state.user);
   const [bookedList, setBookedList] = React.useState([]);
+  const [status, setStatus] = React.useState(statusList[0]);
   const fetchData = async () => {
     try {
-      const res = await privateAxios.get(`bedRequest/user/${user.id}`);
+      const res = await privateAxios.get(`bed-request/user/${user.id}`);
       console.log(res.data);
-      setBookedList[res.data];
+      setBookedList(res.data);
     } catch (error) {
       console.log(error);
     }
@@ -40,36 +38,65 @@ export default function BookedHistory() {
     <Box padding={1}>
       <Box display={"flex"} sx={{ justifyContent: "space-between" }}>
         <h1 style={{ marginLeft: "8px" }}>Bed Booked History</h1>
+        <FormControl sx={{ minWidth: 120, m: 1 }}>
+          <InputLabel id="roomType-label">Room Type</InputLabel>
+          <Select
+            id="select"
+            label="Room Type"
+            value={status}
+            labelId="roomType-label"
+            onChange={handleChange}
+          >
+            <MenuItem value="All">All</MenuItem>
+            {types.map((type) => {
+              return (
+                <MenuItem key={type.roomTypeId} value={type.roomTypeId}>
+                  {type.roomTypeName}
+                </MenuItem>
+              );
+            })}
+          </Select>
+        </FormControl>
       </Box>
       <TableContainer component={Paper}>
         <Table sx={{ minWidth: 650 }} aria-label="simple table">
           <TableHead>
             <TableRow>
               <TableCell>ID</TableCell>
-              <TableCell align="right">Bed</TableCell>
-              <TableCell align="right">Price</TableCell>
-              <TableCell align="right">Created Date</TableCell>
-              <TableCell align="right">Returned Date</TableCell>
-              <TableCell align="right">Status</TableCell>
+              <TableCell>Bed</TableCell>
+              <TableCell>Price</TableCell>
+              <TableCell>Created Date</TableCell>
+              <TableCell>Returned Date</TableCell>
+              <TableCell>Status</TableCell>
+              <TableCell>View</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
             {bookedList.map((bookedRequest) => (
               <TableRow
-                key={bookedRequest.bookedRequestId}
+                key={bookedRequest.bedRequestId}
                 sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
               >
                 <TableCell component="th" scope="row">
-                  {bookedRequest.bed}
+                  {bookedRequest.bedRequestId}
                 </TableCell>
-                <TableCell align="right">{bookedRequest.price}</TableCell>
-                <TableCell align="right">{bookedRequest.createdDate}</TableCell>
-                <TableCell align="right">
-                  {bookedRequest.updatedDate
-                    ? bookedRequest.updatedDate
-                    : "N/A"}
+                <TableCell>{bookedRequest.bedName}</TableCell>
+                <TableCell>{bookedRequest.price}</TableCell>
+                <TableCell>{bookedRequest.createdAt}</TableCell>
+                <TableCell>
+                  {bookedRequest.updatedAt ? bookedRequest.updatedAt : "N/A"}
                 </TableCell>
-                <TableCell align="right">{bookedRequest.status}</TableCell>
+                <TableCell>{bookedRequest.status}</TableCell>
+                <TableCell>
+                  <IconButton>
+                    <RemoveRedEye />
+                  </IconButton>
+                  {bookedRequest.status === "pending" && (
+                    <IconButton>
+                      <Remove />
+                    </IconButton>
+                  )}
+                </TableCell>
               </TableRow>
             ))}
           </TableBody>
