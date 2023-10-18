f84beb9cd8e20339d51892e25190e65505b72a69
loinq2704 <129748596+loinq2704@users.noreply.github.com>
2023-10-18 11:11:08 +0700

    Revert "Revert "Student request""

--- src/pages/student/bookedhistory/BookedHistory.jsx
+++ src/pages/student/bookedhistory/BookedHistory.jsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import { useEffect, useState } from "react";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
@@ -8,71 +8,171 @@ import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import Box from "@mui/material/Box";
 import { privateAxios } from "../../../service/axios";
+import {
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  FormControl,
+  Grid,
+  IconButton,
+  InputLabel,
+  MenuItem,
+  Pagination,
+  Select,
+  Typography,
+} from "@mui/material";
+import { Remove, RemoveRedEye } from "@mui/icons-material";
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
 
-const rows = [
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-  createData("Eclair", 262, 16.0, 24, 6.0),
-  createData("Cupcake", 305, 3.7, 67, 4.3),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
-];
+const statusList = ["pending", "approved", "rejected"];
+const user = JSON.parse(localStorage.getItem("user"));
 export default function BookedHistory() {
-  const [bookedList, setBookedList] = React.useState([]);
+  const [bookedList, setBookedList] = useState([]);
+  const [totalPages, setTotalPages] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [status, setStatus] = useState("");
+  const [reload, setReload] = useState(false);
+  const [open, setOpen] = useState(false);
+  const handleChange = (e) => {
+    if (e.target.value === "All") setStatus("");
+    else setStatus(e.target.value);
+  };
+  const handleCanelBooking = (id) => {
+    if (confirm(`Room Type ID ${id} will be delete?`)) {
+      (async () => {
+        await privateAxios.delete(`bed-request/${id}`);
+        await fetchData();
+      })();
+    }
+    console.log(id);
+  };
   const fetchData = async () => {
     try {
-      const res = await privateAxios.get(`bedRequest/user/${user.id}`);
+      const res = await privateAxios.get(
+        `bed-request/user/${user.id}?status=${status}&page=${currentPage - 1}`
+      );
       console.log(res.data);
-      setBookedList[res.data];
+      setBookedList(res.data.data);
+      setTotalPages(res.data.totalPages);
     } catch (error) {
       console.log(error);
     }
   };
-  React.useEffect(() => {
+  useEffect(() => {
     fetchData();
-  }, []);
+  }, [currentPage, status, reload]);
   return (
     <Box padding={1}>
       <Box display={"flex"} sx={{ justifyContent: "space-between" }}>
         <h1 style={{ marginLeft: "8px" }}>Bed Booked History</h1>
+        <FormControl sx={{ minWidth: 120, m: 1 }}>
+          <InputLabel id="roomType-label">Status</InputLabel>
+          <Select
+            id="select"
+            label="Status"
+            value={status === "" ? "All" : status}
+            labelId="roomType-label"
+            onChange={handleChange}
+          >
+            <MenuItem value="All">All</MenuItem>
+            {statusList.map((type) => {
+              return (
+                <MenuItem key={type} value={type.toLowerCase()}>
+                  {type.toLowerCase()}
+                </MenuItem>
+              );
+            })}
+          </Select>
+        </FormControl>
       </Box>
       <TableContainer component={Paper}>
         <Table sx={{ minWidth: 650 }} aria-label="simple table">
           <TableHead>
             <TableRow>
               <TableCell>ID</TableCell>
-              <TableCell align="right">Bed</TableCell>
-              <TableCell align="right">Price</TableCell>
-              <TableCell align="right">Created Date</TableCell>
-              <TableCell align="right">Returned Date</TableCell>
-              <TableCell align="right">Status</TableCell>
+              <TableCell>Bed</TableCell>
+              <TableCell>Price</TableCell>
+              <TableCell>Created Date</TableCell>
+              <TableCell>Returned Date</TableCell>
+              <TableCell>Status</TableCell>
+              <TableCell>Setting</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
             {bookedList.map((bookedRequest) => (
               <TableRow
-                key={bookedRequest.bookedRequestId}
+                key={bookedRequest.bedRequestId}
                 sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
               >
                 <TableCell component="th" scope="row">
-                  {bookedRequest.bed}
+                  {bookedRequest.bedRequestId}
                 </TableCell>
-                <TableCell align="right">{bookedRequest.price}</TableCell>
-                <TableCell align="right">{bookedRequest.createdDate}</TableCell>
-                <TableCell align="right">
-                  {bookedRequest.updatedDate
-                    ? bookedRequest.updatedDate
+                <TableCell>{bookedRequest.bedName}</TableCell>
+                <TableCell>{bookedRequest.price}</TableCell>
+                <TableCell>{bookedRequest.createdAt}</TableCell>
+                <TableCell>
+                  {bookedRequest.status === "Approved"
+                    ? bookedRequest.updatedAt
                     : "N/A"}
                 </TableCell>
-                <TableCell align="right">{bookedRequest.status}</TableCell>
+                <TableCell>{bookedRequest.status}</TableCell>
+                <TableCell>
+                  {/* <IconButton onClick={() => setOpen(true)}>
+                    <RemoveRedEye />
+                  </IconButton> */}
+                  {bookedRequest.status === "pending" && (
+                    <IconButton
+                      // data-itemID={bookedRequest.bedRequestId}
+                      onClick={() =>
+                        handleCanelBooking(bookedRequest.bedRequestId)
+                      }
+                    >
+                      <Remove />
+                    </IconButton>
+                  )}
+                </TableCell>
               </TableRow>
             ))}
           </TableBody>
         </Table>
       </TableContainer>
+      <Pagination
+        color="primary"
+        count={totalPages}
+        page={currentPage}
+        onChange={(e, value) => {
+          setCurrentPage(value);
+        }}
+        sx={{
+          justifyContent: "center",
+          "& .MuiPagination-ul": {
+            justifyContent: "center",
+          },
+          "&& .Mui-selected": {
+            bgcolor: "orangered",
+          },
+          "& .MuiPaginationItem-root:hover": {
+            bgcolor: "rgba(255,69,0,0.8)",
+          },
+          "&& .Mui-selected:hover": {
+            bgcolor: "rgba(255,69,0,0.8)",
+          },
+          my: 4,
+        }}
+      />
+      <Dialog open={open} onClose={() => setOpen(false)}>
+        <DialogTitle>Booked Details</DialogTitle>
+        <DialogContent>
+          <Grid container spacing={2}>
+            <Grid item xs={3}>
+              Bed:
+            </Grid>
+            <Grid item xs={9}>
+              asdf
+            </Grid>
+          </Grid>
+        </DialogContent>
+      </Dialog>
     </Box>
   );
 }
