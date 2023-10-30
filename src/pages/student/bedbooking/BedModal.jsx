074e0c715699a206ee37821a02d31a0e0a06351b
ThaoTTP123 <dreemmurrchara@gmail.com>
2023-10-30 09:41:49 +0700

    bed-request + payment

--- src/pages/student/bedbooking/BedModal.jsx
+++ src/pages/student/bedbooking/BedModal.jsx
@@ -10,6 +10,7 @@ import {
 } from "@mui/material";
 import SingleBedIcon from "@mui/icons-material/SingleBed";
 import { privateAxios } from "../../../service/axios";
+import BookedStatusModal from "./BookedStatusModal";
 
 export default function BedModal({
   roomId,
@@ -20,8 +21,11 @@ export default function BedModal({
 }) {
   const [bedList, setBedList] = useState([]);
   const [selectedBed, setSelectedBed] = useState(null);
+  const [bookedStatus, setBookedStatus] = useState(false);
+  const [openStatus, setOpenStatus] = useState(false);
+  const [reload, setReload] = useState(false);
   const handleBedClick = (bed) => {
-    if (bed.status !== "occupied") {
+    if (bed.status.toLowerCase() === "vacant") {
       setSelectedBed(bed);
     }
   };
@@ -29,9 +33,22 @@ export default function BedModal({
   const handleConfirm = (bed) => {
     // Handle the confirmation action here
     if (selectedBed && confirm(`Confirmed Booking: ${selectedBed.bedName}?`)) {
-      privateAxios.post(
-        `bed-request?studentId=${studentId}&bedId=${bed.id}&semesterId=${semesterId}`
-      );
+      privateAxios
+        .post(
+          `bed-request/book?studentId=${studentId}&bedId=${bed.id}&semesterId=${semesterId}`
+        )
+        .then((response) => {
+          if (response.status === 201) {
+            setBookedStatus(true);
+            setOpen(false);
+            setOpenStatus(true);
+          }
+        })
+        .catch((error) => {
+          setBookedStatus(false);
+          setOpen(false);
+          setOpenStatus(true);
+        });
       // You can perform further actions here, e.g., submit data or update the state.
     }
   };
@@ -41,78 +58,94 @@ export default function BedModal({
   };
   useEffect(() => {
     fetchBed();
-  }, []);
+  }, [reload]);
   return (
-    <div>
-      <Modal open={open} onClose={() => setOpen(false)}>
-        <Box
-          sx={{
-            position: "absolute",
-            top: "50%",
-            left: "50%",
-            transform: "translate(-50%, -50%)",
-            width: 400,
-            bgcolor: "background.paper",
-            boxShadow: 24,
-            p: 4,
-            overflowY: "auto", // Enable vertical scrolling
-            maxHeight: "80vh", // Set a maximum height to enable scrolling
+    <>
+      <div>
+        <Modal
+          open={open}
+          onClose={() => {
+            setOpen(false);
           }}
         >
-          <Grid container spacing={2} sx={{ xs: 12, md: 4 }}>
-            {bedList.map((bed) => (
-              <Grid item xs={12} md={3} textAlign={"center"} key={bed.id}>
-                {bed.bedName}
-                <IconButton
-                  sx={{
-                    "& .MuiSvgIcon-root": {
-                      fill:
-                        bed.status.toLowerCase() === "vacant"
-                          ? "currentColor"
-                          : bed.status.toLowerCase() === "reserved"
-                          ? "yellow"
-                          : "red",
-                      width: "50px",
-                      height: "50px",
-                    },
-                    cursor:
-                      bed.status.toLowerCase() === "occupied" ||
-                      bed.status.toLowerCase() === "reserved"
-                        ? "not-allowed"
-                        : "pointer",
-                    "&.selected": {
+          <Box
+            sx={{
+              position: "absolute",
+              top: "50%",
+              left: "50%",
+              transform: "translate(-50%, -50%)",
+              width: 400,
+              bgcolor: "background.paper",
+              boxShadow: 24,
+              p: 4,
+              overflowY: "auto", // Enable vertical scrolling
+              maxHeight: "80vh", // Set a maximum height to enable scrolling
+            }}
+          >
+            <Grid container spacing={2} sx={{ xs: 12, md: 4 }}>
+              {bedList.map((bed) => (
+                <Grid item xs={12} md={3} textAlign={"center"} key={bed.id}>
+                  {bed.bedName}
+                  <IconButton
+                    sx={{
                       "& .MuiSvgIcon-root": {
-                        fill: "green",
+                        fill:
+                          bed.status.toLowerCase() === "vacant"
+                            ? "currentColor"
+                            : bed.status.toLowerCase() === "reserved"
+                            ? "yellow"
+                            : "red",
+                        width: "50px",
+                        height: "50px",
                       },
-                    },
-                  }}
-                  className={selectedBed === bed ? "selected" : ""}
-                  onClick={() => handleBedClick(bed)}
+                      cursor:
+                        bed.status.toLowerCase() === "occupied" ||
+                        bed.status.toLowerCase() === "reserved"
+                          ? "not-allowed !important"
+                          : "pointer",
+                      "&.selected": {
+                        "& .MuiSvgIcon-root": {
+                          fill: "green",
+                        },
+                      },
+                    }}
+                    className={selectedBed === bed ? "selected" : ""}
+                    onClick={() => handleBedClick(bed)}
+                  >
+                    <SingleBedIcon />
+                  </IconButton>
+                </Grid>
+              ))}
+            </Grid>
+            {selectedBed && (
+              <div>
+                <Typography variant="h6">
+                  Selected Bed: {selectedBed.bedName}
+                </Typography>
+                <Typography variant="body1">
+                  Status: {selectedBed.status}
+                </Typography>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  onClick={() => handleConfirm(selectedBed)}
                 >
-                  <SingleBedIcon />
-                </IconButton>
-              </Grid>
-            ))}
-          </Grid>
-          {selectedBed && (
-            <div>
-              <Typography variant="h6">
-                Selected Bed: {selectedBed.bedName}
-              </Typography>
-              <Typography variant="body1">
-                Status: {selectedBed.status}
-              </Typography>
-              <Button
-                variant="contained"
-                color="primary"
-                onClick={() => handleConfirm(selectedBed)}
-              >
-                Book
-              </Button>
-            </div>
-          )}
-        </Box>
-      </Modal>
-    </div>
+                  Book
+                </Button>
+              </div>
+            )}
+          </Box>
+        </Modal>
+      </div>
+      <div>
+        {openStatus && (
+          <BookedStatusModal
+            open={openStatus}
+            setOpen={setOpenStatus}
+            status={bookedStatus}
+          />
+        )}
+      </div>
+    </>
   );
 }
