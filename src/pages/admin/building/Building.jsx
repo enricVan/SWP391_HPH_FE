9f2ccc38a3783269c2bd45feaa6eea4eef02a358
ThaoTTP123 <dreemmurrchara@gmail.com>
2023-11-10 01:52:54 +0700

    crud room,roomType,building

--- src/pages/admin/building/Building.jsx
+++ src/pages/admin/building/Building.jsx
@@ -1,17 +1,26 @@
-import { useEffect, useState } from "react";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
-import Box from "@mui/material/Box";
-import { privateAxios } from "../../../service/axios";
+import { useEffect, useState } from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import AddCircle from '@mui/icons-material/AddCircle';
+import Delete from '@mui/icons-material/Delete';
+import Edit from '@mui/icons-material/Edit';
+import { privateAxios } from '../../../service/axios';
+import BuildingForm from './BuildingForm';
 
 export default function Building() {
   const [buildings, setBuildings] = useState([]);
-
+  const [selectedBuilding, setSelectedBuilding] = useState(null);
+  const [openAdd, setOpenAdd] = useState(false);
+  const [openEdit, setOpenEdit] = useState(false);
+  const [reload, setReload] = useState(false);
   const fetchData = async () => {
     try {
       const res = await privateAxios.get(`building/admin`);
@@ -24,72 +33,121 @@ export default function Building() {
   };
   useEffect(() => {
     fetchData();
-  }, []);
+  }, [reload]);
 
   return (
     <Box padding={2}>
       <Box>
         <div
           style={{
-            backgroundColor: "#034EA2",
-            padding: "6px",
-            borderRadius: "15px",
-            marginBottom: "10px",
+            backgroundColor: '#034EA2',
+            padding: '6px',
+            borderRadius: '15px',
+            marginBottom: '10px',
           }}
         >
           <h1
             style={{
-              textAlign: "center",
-              fontWeight: "bold",
-              color: "#fff",
-              textTransform: "uppercase",
-              margin: "0",
+              textAlign: 'center',
+              fontWeight: 'bold',
+              color: '#fff',
+              textTransform: 'uppercase',
+              margin: '0',
             }}
           >
             Building
           </h1>
         </div>
       </Box>
-
+      <Box sx={{ display: 'flex', justifyContent: 'right', mb: 2 }}>
+        <Button
+          variant='contained'
+          endIcon={<AddCircle />}
+          onClick={() => setOpenAdd(true)}
+        >
+          Create
+        </Button>
+      </Box>
       <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-          <TableHead sx={{ backgroundColor: "#FF5800" }}>
+        <Table sx={{ minWidth: 650 }} aria-label='simple table'>
+          <TableHead sx={{ backgroundColor: '#FF5800' }}>
             <TableRow>
-              <TableCell style={{ fontWeight: "bold", color: "#fff" }}>
+              <TableCell style={{ fontWeight: 'bold', color: '#fff' }}>
                 ID
               </TableCell>
-              <TableCell style={{ fontWeight: "bold", color: "#fff" }}>
+              <TableCell style={{ fontWeight: 'bold', color: '#fff' }}>
                 Building Name
               </TableCell>
-              <TableCell style={{ fontWeight: "bold", color: "#fff" }}>
+              <TableCell style={{ fontWeight: 'bold', color: '#fff' }}>
                 Number of Floor
               </TableCell>
-              <TableCell style={{ fontWeight: "bold", color: "#fff" }}>
+              <TableCell style={{ fontWeight: 'bold', color: '#fff' }}>
                 Created Date
               </TableCell>
-              <TableCell style={{ fontWeight: "bold", color: "#fff" }}>
+              <TableCell style={{ fontWeight: 'bold', color: '#fff' }}>
                 Updated Date
+              </TableCell>
+              <TableCell
+                colSpan={2}
+                sx={{ color: 'white', fontWeight: 'bold' }}
+              >
+                Settings
               </TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
             {buildings.map((b) => (
               <TableRow
-                key={b.buildingId}
-                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                key={b.id}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
               >
-                <TableCell component="th" scope="row">
+                <TableCell component='th' scope='row'>
                   {b.id}
                 </TableCell>
                 <TableCell>{b.buildingName}</TableCell>
                 <TableCell>{b.numberFloor}</TableCell>
                 <TableCell>{b.createdAt}</TableCell>
                 <TableCell>{b.updateAt}</TableCell>
+                <TableCell>
+                  <IconButton
+                    onClick={() => {
+                      setSelectedBuilding(b);
+                      setOpenEdit(true);
+                    }}
+                    variant='contained'
+                  >
+                    <Edit />
+                  </IconButton>
+                  <IconButton
+                    onClick={() => {
+                      if (
+                        confirm(`Do you want to delete building ID ${b.id}`)
+                      ) {
+                        privateAxios.delete(`building/${b.id}`).then((res) => {
+                          alert(res.data);
+                          setReload(!reload);
+                        });
+                      }
+                    }}
+                    variant='contained'
+                  >
+                    <Delete />
+                  </IconButton>
+                </TableCell>
               </TableRow>
             ))}
           </TableBody>
         </Table>
       </TableContainer>
+      {(openAdd || openEdit) && (
+        <BuildingForm
+          reload={reload}
+          setReload={setReload}
+          building={openAdd ? null : selectedBuilding}
+          open={openAdd ? openAdd : openEdit}
+          setOpen={openAdd ? setOpenAdd : setOpenEdit}
+        />
+      )}
     </Box>
   );
 }
--- src/pages/admin/user/ManagerUser.jsx
+++ src/pages/admin/user/ManagerUser.jsx
@@ -1,5 +1,171 @@
-import React from "react";
-
+import React, { useEffect, useState } from 'react';
+import { privateAxios } from '../../../service/axios';
+import Box from '@mui/material/Box';
+import TableContainer from '@mui/material/TableContainer';
+import Table from '@mui/material/Table';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import TableCell from '@mui/material/TableCell';
+import TableBody from '@mui/material/TableBody';
+import Paper from '@mui/material/Paper';
+import IconButton from '@mui/material/IconButton';
+import Button from '@mui/material/Button';
+import Edit from '@mui/icons-material/Edit';
+import Delete from '@mui/icons-material/Delete';
+import AddCircle from '@mui/icons-material/AddCircle';
+import SearchIcon from '@mui/icons-material/Search';
+import { useDispatch, useSelector } from 'react-redux';
+import { open } from '../../../features/userFormSlice';
+import Searchbar from '../../../components/Searchbar';
+import RemoveRedEye from '@mui/icons-material/RemoveRedEye';
+import { useNavigate } from 'react-router-dom';
+import { Pagination } from '@mui/material';
+import ManagerForm from './form/ManagerForm';
+const { Search, SearchIconWrapper, StyledInputBase } = Searchbar;
 export default function ManagerUser() {
-  return <div>ManagerUser</div>;
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { openAddManager } = useSelector((state) => state.userForm);
+  const [openEdit, setOpenEdit] = useState(false);
+  const [managers, setManagers] = useState([]);
+  const [selectedUser, setSelectedUser] = useState(null);
+  const [totalPages, setTotalPages] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [reload, setReload] = useState(false);
+  const [search, setSearch] = useState('');
+  const fetchManager = async () => {
+    const res = await privateAxios.get(`manager?rollNumber=${search}`);
+    setManagers(res.data.data);
+
+    setTotalPages(res.totalPages);
+  };
+  useEffect(() => {
+    fetchManager();
+    setCurrentPage(1);
+  }, [search, reload]);
+  useEffect(() => {
+    fetchManager();
+  }, [currentPage]);
+  return (
+    <Box padding={1}>
+      <Search sx={{ display: 'inline-block' }}>
+        <SearchIconWrapper>
+          <SearchIcon />
+        </SearchIconWrapper>
+        <StyledInputBase
+          onChange={(e) => {
+            setTimeout(() => {
+              setSearch(e.target.value);
+            }, 700)();
+          }}
+          placeholder='Roll Number...'
+          inputProps={{ 'aria-label': 'search' }}
+          sx={{
+            border: '5px solid orangered',
+            borderRadius: '30px',
+          }}
+        />
+      </Search>
+      <Box textAlign={'right'} mb={2}>
+        <Button
+          onClick={() => {
+            dispatch(open('ADD_MANAGER'));
+          }}
+          variant='contained'
+          endIcon={<AddCircle />}
+        >
+          Create
+        </Button>
+      </Box>
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow sx={{ backgroundColor: 'orangered' }}>
+              <TableCell sx={{ color: 'white', fontWeight: 'bold' }}>
+                ID
+              </TableCell>
+              <TableCell sx={{ color: 'white', fontWeight: 'bold' }}>
+                Description
+              </TableCell>
+              <TableCell sx={{ color: 'white', fontWeight: 'bold' }}>
+                FullName
+              </TableCell>
+              <TableCell sx={{ color: 'white', fontWeight: 'bold' }}>
+                Created Date
+              </TableCell>
+              <TableCell sx={{ color: 'white', fontWeight: 'bold' }}>
+                Updated Date
+              </TableCell>
+              <TableCell
+                colSpan={2}
+                sx={{ color: 'white', fontWeight: 'bold' }}
+              >
+                Settings
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {/* {managers.map((manager) => (
+              <TableRow key={manager.id}>
+                <TableCell component='th'>{manager.id}</TableCell>
+                <TableCell>{manager.rollNumber}</TableCell>
+                <TableCell>{manager.userDto.fullName}</TableCell>
+                <TableCell>{manager.userDto.address}</TableCell>
+                <TableCell>{manager.createdAt}</TableCell>
+                <TableCell>{manager.updateAt}</TableCell>
+                <TableCell>
+                  <IconButton onClick={() => {}} variant='contained'>
+                    <Edit />
+                  </IconButton>
+                  <IconButton onClick={() => {}} variant='contained'>
+                    <Delete />
+                  </IconButton>
+                  <IconButton
+                    onClick={() => {
+                      navigate(`${manager.rollNumber}`);
+                    }}
+                    variant='contained'
+                  >
+                    <RemoveRedEye />
+                  </IconButton>
+                </TableCell>
+              </TableRow>
+            ))} */}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <Pagination
+        color='primary'
+        count={totalPages}
+        page={currentPage}
+        onChange={(e, value) => {
+          setCurrentPage(value);
+        }}
+        sx={{
+          justifyContent: 'center',
+          '& .MuiPagination-ul': {
+            justifyContent: 'center',
+          },
+          '&& .Mui-selected': {
+            bgcolor: 'orangered',
+          },
+          '& .MuiPaginationItem-root:hover': {
+            bgcolor: 'rgba(255,69,0,0.8)',
+          },
+          '&& .Mui-selected:hover': {
+            bgcolor: 'rgba(255,69,0,0.8)',
+          },
+          my: 4,
+        }}
+      />
+      {openAddManager && <ManagerForm reload={reload} setReload={setReload} />}
+      {openEdit && (
+        <EditUser
+          openEdit={openEdit}
+          setOpenEdit={setOpenEdit}
+          user={selectedUser}
+        />
+      )}
+    </Box>
+  );
 }
