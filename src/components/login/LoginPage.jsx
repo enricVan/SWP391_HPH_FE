d6e66cf99b27d1fae74baebc05f8617366cf87c6
ThaoTTP123 <dreemmurrchara@gmail.com>
2023-11-15 03:06:51 +0700

    cd news

--- src/components/login/LoginPage.jsx
+++ src/components/login/LoginPage.jsx
@@ -8,14 +8,13 @@ import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
 export function LoginPage() {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const { user, isError, isSuccess, message } = useSelector(
     (state) => state.auth
   );
   const schema = yup.object().shape({
-    name: yup.string().required(),
+    username: yup.string().required(),
     password: yup.string().required(),
-  const navigate = useNavigate();
-
   });
   const {
     register,
@@ -24,44 +23,43 @@ export function LoginPage() {
     reset,
   } = useForm({
     defaultValues: {
-      name: '',
+      username: '',
       password: '',
     },
     resolver: yupResolver(schema),
   });
-  const navigate = useNavigate();
   function onSubmit(data) {
     dispatch(login(data));
     reset({ keepValues: true });
   }
   useEffect(() => {
-    if (user && user !== "inactive") {
-      const path = "/" + user.roleName.toLowerCase();
+    if (user && user !== 'inactive') {
+      const path = '/' + user.roleName.toLowerCase();
       navigate(path);
     }
   }, [user, isError, isSuccess, message, dispatch, navigate]);
   return (
     <>
-      <div className="container d-flex justify-content-center align-items-center min-vh-100 body-bg">
-        <div className="row border rounder-5 p-3 bg-white shadow box-area">
-          <div className="col-md-6 left-box rounder-4 d-flex justify-content-center align-items-center flex-column">
-            <div className="featured-image mb-3">
+      <div className='container d-flex justify-content-center align-items-center min-vh-100 body-bg'>
+        <div className='row border rounder-5 p-3 bg-white shadow box-area'>
+          <div className='col-md-6 left-box rounder-4 d-flex justify-content-center align-items-center flex-column'>
+            <div className='featured-image mb-3'>
               <img
-                src="src/assets/image/logo-moi.png"
-                className="img-fluid"
-                style={{ width: "100%" }}
+                src='src/assets/image/logo-moi.png'
+                className='img-fluid'
+                style={{ width: '100%' }}
               />
             </div>
           </div>
-          <div className="col-md-6 right-box">
-            <div className="row justify-content-center">
-              <div className="header-text mb-4">
-                <h3 className="format-text" style={{ fontWeight: "bold" }}>
+          <div className='col-md-6 right-box'>
+            <div className='row justify-content-center'>
+              <div className='header-text mb-4'>
+                <h3 className='format-text' style={{ fontWeight: 'bold' }}>
                   Welcome To Dormitory FPTU
                 </h3>
               </div>
-              <div className="input-group mb-3 form-control-lg justify-content-center">
-                <h3 className="format-text" style={{ fontWeight: "bold" }}>
+              <div className='input-group mb-3 form-control-lg justify-content-center'>
+                <h3 className='format-text' style={{ fontWeight: 'bold' }}>
                   CAMPUS HOLA
                 </h3>
               </div>
@@ -71,11 +69,11 @@ export function LoginPage() {
                     type='text'
                     className='form-control form-control-lg bg-light fs-6'
                     placeholder='Username'
-                    {...register('name')}
+                    {...register('username')}
                   />
                 </div>
-                {isSubmitted && errors.name && (
-                  <p style={{ color: 'red' }}>{errors.name.message}</p>
+                {isSubmitted && errors.username && (
+                  <p style={{ color: 'red' }}>{errors.username.message}</p>
                 )}
                 <div className='input-group mb-3'>
                   <input
@@ -95,25 +93,23 @@ export function LoginPage() {
                 ) : isValid && isSubmitted && user === 'inactive' ? (
                   <p style={{ color: 'red', fontSize: '13px' }}>
                     {'Account is not allowed!'}
-
                   </p>
                 ) : (
-                  ""
+                  ''
                 )}
-                <div className="input-group mb-3 d-flex justify-content-between">
+                <div className='input-group mb-3 d-flex justify-content-between'>
                   <div></div>
-                  <div className="forget">
+                  <div className='forget'>
                     <small>
-                      <Link to="/forgetPassword">Forget Password?</Link>
+                      <Link to='/forgetPassword'>Forget Password?</Link>
                     </small>
                   </div>
                 </div>
-                <div className="input-group mb-3">
+                <div className='input-group mb-3'>
                   <button
                     type='submit'
                     className='btn btn-lg login w-100 fs-6 font-text'
                     style={{ backgroundColor: 'orangered', color: 'white' }}
-
                   >
                     Login
                   </button>
--- src/pages/manager/news/News.jsx
+++ src/pages/manager/news/News.jsx
@@ -1,9 +1,10 @@
-import { useEffect, useState } from "react";
-import Box from "@mui/material/Box";
-import Grid from "@mui/material/Grid";
-import SearchIcon from "@mui/icons-material/Search";
-import Searchbar from "../../../components/Searchbar";
+import { useEffect, useState } from 'react';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import SearchIcon from '@mui/icons-material/Search';
+import Searchbar from '../../../components/Searchbar';
 import {
+  Button,
   Card,
   CardActionArea,
   CardContent,
@@ -11,9 +12,12 @@ import {
   Pagination,
   Typography,
   styled,
-} from "@mui/material";
-import { Link } from "react-router-dom";
-import { privateAxios } from "../../../service/axios";
+} from '@mui/material';
+import { Link } from 'react-router-dom';
+import { privateAxios } from '../../../service/axios';
+import NewsForm from './NewsForm';
+import { Add, Delete } from '@mui/icons-material';
+import { useConfirm } from 'material-ui-confirm';
 const { Search, SearchIconWrapper, StyledInputBase } = Searchbar;
 const StyledLink = styled(Link)`
   text-decoration: none;
@@ -27,10 +31,13 @@ const StyledLink = styled(Link)`
   }
 `;
 export default function News() {
+  const muiConfirm = useConfirm();
   const [news, setNews] = useState([]);
   const [totalPages, setTotalPages] = useState(0);
   const [currentPage, setCurrentPage] = useState(1);
-  const [searchTitle, setSearchTitle] = useState("");
+  const [searchTitle, setSearchTitle] = useState('');
+  const [openAdd, setOpenAdd] = useState(false);
+  const [reload, setReload] = useState(false);
   const fetchData = async () => {
     const res = await privateAxios.get(
       `news?title=${searchTitle}&page=${currentPage - 1}`
@@ -40,31 +47,31 @@ export default function News() {
   };
   useEffect(() => {
     fetchData();
-  }, [currentPage, searchTitle]);
+  }, [currentPage, searchTitle, reload]);
   return (
     <Box p={2}>
       <div
         style={{
-          backgroundColor: "#034EA2",
-          padding: "6px",
-          borderRadius: "15px",
-          marginBottom: "10px",
+          backgroundColor: '#034EA2',
+          padding: '6px',
+          borderRadius: '15px',
+          marginBottom: '10px',
         }}
       >
         <h2
           style={{
-            textAlign: "center",
-            fontWeight: "bold",
-            color: "#fff",
-            textTransform: "uppercase",
-            margin: "0",
+            textAlign: 'center',
+            fontWeight: 'bold',
+            color: '#fff',
+            textTransform: 'uppercase',
+            margin: '0',
           }}
         >
           News
         </h2>
       </div>
-      <Box flex>
-        <Search sx={{ display: "inline-block" }}>
+      <Box flex justifyContent={'space-around'}>
+        <Search sx={{ display: 'inline-block' }}>
           <SearchIconWrapper>
             <SearchIcon />
           </SearchIconWrapper>
@@ -72,34 +79,57 @@ export default function News() {
             onChange={(e) => {
               setSearchTitle(e.target.value);
             }}
-            placeholder="Search…"
-            inputProps={{ "aria-label": "search" }}
+            placeholder='Search…'
+            inputProps={{ 'aria-label': 'search' }}
             sx={{
-              border: "5px solid orangered",
-              borderRadius: "30px",
+              border: '5px solid orangered',
+              borderRadius: '30px',
             }}
           />
         </Search>
+        <Button
+          variant='contained'
+          endIcon={<Add />}
+          onClick={() => setOpenAdd(true)}
+        >
+          Add
+        </Button>
       </Box>
-      <Grid container p={"8px"} spacing={1}>
+      <Grid container p={'8px'} spacing={1}>
         {news.map((item) => {
           return (
             <Grid item xs={6} my={2} key={item.newsId}>
-              <Card sx={{ width: "100%", height: "100%" }}>
+              <span
+                style={{ color: 'red', float: 'right', cursor: 'pointer' }}
+                onClick={() => {
+                  muiConfirm({
+                    title: 'Delete News?',
+                    content: `Are you sure you want to delete news ID ${item.newsId}`,
+                  }).then((res) => {
+                    privateAxios.delete(`news/${item.newsId}`).then((res) => {
+                      alert(res.data);
+                      setReload(!reload);
+                    });
+                  });
+                }}
+              >
+                <Delete />
+              </span>
+              <Card sx={{ width: '100%', height: '100%' }}>
                 <StyledLink to={`detail/${item.newsId}`}>
                   <CardActionArea>
                     <CardContent>
                       <Typography
                         gutterBottom
-                        variant="h7"
-                        component="div"
-                        fontWeight={"bold"}
-                        color={"gray"}
+                        variant='h7'
+                        component='div'
+                        fontWeight={'bold'}
+                        color={'gray'}
                       >
                         {item.createdAt}
                       </Typography>
-                      <Divider sx={{ bgcolor: "black" }} />
-                      <Typography variant="body2" color="primary" mt={1}>
+                      <Divider sx={{ bgcolor: 'black' }} />
+                      <Typography variant='body2' color='primary' mt={1}>
                         {item.title}
                       </Typography>
                     </CardContent>
@@ -111,29 +141,37 @@ export default function News() {
         })}
       </Grid>
       <Pagination
-        color="primary"
+        color='primary'
         count={totalPages}
         page={currentPage}
         onChange={(e, value) => {
           setCurrentPage(value);
         }}
         sx={{
-          justifyContent: "center",
-          "& .MuiPagination-ul": {
-            justifyContent: "center",
+          justifyContent: 'center',
+          '& .MuiPagination-ul': {
+            justifyContent: 'center',
           },
-          "&& .Mui-selected": {
-            bgcolor: "orangered",
+          '&& .Mui-selected': {
+            bgcolor: 'orangered',
           },
-          "& .MuiPaginationItem-root:hover": {
-            bgcolor: "rgba(255,69,0,0.8)",
+          '& .MuiPaginationItem-root:hover': {
+            bgcolor: 'rgba(255,69,0,0.8)',
           },
-          "&& .Mui-selected:hover": {
-            bgcolor: "rgba(255,69,0,0.8)",
+          '&& .Mui-selected:hover': {
+            bgcolor: 'rgba(255,69,0,0.8)',
           },
           my: 4,
         }}
       />
+      {openAdd && (
+        <NewsForm
+          openAdd={openAdd}
+          setOpenAdd={setOpenAdd}
+          reload={reload}
+          setReload={setReload}
+        />
+      )}
     </Box>
   );
 }
